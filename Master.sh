9060894ada8edc48006dc622586804862501b5f4
bnewson-dunn <bnewson-dunn@nsf.org>
2024-10-18 09:21:28 -0400

    Update Master.sh

--- Master.sh
+++ Master.sh
@@ -1,9 +1,3 @@
-# Disable Swap, Install containerd, Kubernetes components, and Initialize Master Node
-sudo swapoff -a && sudo apt-get update && sudo apt-get install -y containerd apt-transport-https ca-certificates curl
-sudo mkdir -p /etc/containerd && sudo containerd config default | sudo tee /etc/containerd/config.toml && sudo systemctl restart containerd
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubectl && sudo apt-mark hold kubelet kubeadm kubectl
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+kubeadm join <master-ip>:6443 --token <generated-token> --discovery-token-ca-cert-hash sha256:<hash>
+kubectl get nodes
